2e4faf87a3a01d85dd295e812504eac966e1948b
Kota Miyamoto <kotamiyamoto@KotanoMacBook-Pro.local>
2020-03-16 23:57:30 +0900

    add Todo

---
+++ todo-app/src/components/Todo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input type="checkbox"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { createNamespacedHelpers } from 'vuex'
+
+@Component
+export default class Todo extends Vue {
+  @Prop() private id!: string;
+}
+</script>
