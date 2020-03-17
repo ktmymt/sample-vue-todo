b5a70158fdbd0ae1f4e6b3b84fe1c43e553071d4
Kota Miyamoto <kotamiyamoto@KotanoMacBook-Pro.local>
2020-03-17 23:53:05 +0900

    add form

--- todo-app/src/views/Home.vue
+++ todo-app/src/views/Home.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+
+    <div>
+      <label for="title">Title: </label>
+      <input type="text" v-model="title" placeholder="Title" />
+      <label for="body">Body: </label>
+      <textarea v-model="body" placeholder="Body"></textarea>
+    </div>
+    <div>
+      <ul v-for="(list, key) in lists" :key="key">
+        <li>key: {{ key }} -> {{ list.title }}</li>
+      </ul>
+    </div>
+    <div>
+      <pre>
+        {{ $data }}
+      </pre>
+    </div>
   </div>
 </template>
 
-<script>
-
+<script lang="ts">
 export default {
   name: "Home",
+  data: () => ({
+    lists: [{ title: "test todo", body: "test body text" }]
+  })
 };
 </script>
